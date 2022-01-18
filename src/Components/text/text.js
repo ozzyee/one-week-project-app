32d824232514aaa2e2cde14dcb433e0f4bdcd15f
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-01-18 21:50:03 +0000

    fix:for build

--- src/Components/text/text.js
+++ src/Components/text/text.js
@@ -1,11 +1,11 @@
 import "./style.css";
 
 function Text({ type, children }) {
-   if (type == "title") {
+   if (type === "title") {
       return <h1 className="title">{children}</h1>;
    }
 
-   if (type == "sub-title") {
+   if (type === "sub-title") {
       return <h2 className="sub-title">{children}</h2>;
    }
 
