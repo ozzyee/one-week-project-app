259694febf6eb71118748259bf69955976e9d60f
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-01-18 22:00:08 +0000

    fix:for build

--- src/Components/SignIn/index.js
+++ src/Components/SignIn/index.js
@@ -18,7 +18,7 @@ function SignIn() {
       <div id="fixed-screen">
          {/* Background */}
          <div className="bg-img-wrapper">
-            <img src="/space-4984262.jpg" className="bg-img" />
+            <img src="/space-4984262.jpg" className="bg-img" alt="bg"/>
             <div className="overlay" />
          </div>
          <section id="login">
