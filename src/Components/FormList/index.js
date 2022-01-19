58aa5e449b822d0e87e4e57974e6e7bd68269d7e
D20CM <scottbrown500@hotmail.com>
2022-01-19 17:06:11 +0000

    style dashboard

--- src/Components/FormList/index.js
+++ src/Components/FormList/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import FormListItem from '../Forms-list-item/index.js';
+import React from "react";
+import FormListItem from "../Forms-list-item/index.js";
 
-
-function FormList({forms}) {
-    return (
-        <ul> {
-        forms.map(function(item) {
-            return <FormListItem item={item} key={item.key} />
-    }) 
-    } </ul>
-    )
+function FormList({ forms }) {
+  return (
+    <ul className="form-list">
+      {" "}
+      {forms.map(function (item) {
+        return <FormListItem item={item} key={item.key} />;
+      })}{" "}
+    </ul>
+  );
 }
 
 export default FormList;
--- src/Components/Forms-list-item/index.js
+++ src/Components/Forms-list-item/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-function FormListItem({item}) {
-    return (
-        <div>
-            <li>
-                <p>{item.formtitle}</p>
-                <p>{item.date}</p>
-            </li>
-        </div>
-    )
+function FormListItem({ item }) {
+  return (
+    <div>
+      <li className="form-list-item">
+        <p>{item.formtitle}</p>
+        <p>{item.date}</p>
+      </li>
+    </div>
+  );
 }
 
 export default FormListItem;
