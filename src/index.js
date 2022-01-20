69b19d5a1c87a50e1124d99d60ecabc59a316996
D20CM <scottbrown500@hotmail.com>
2022-01-20 23:31:38 +0000

    redundant branch - delete after stand up tomorrow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 import App from "../src/pages/app/App";
@@ -10,16 +10,15 @@ import reportWebVitals from "./reportWebVitals";
 import { ContextProvider } from "../src/auth/auth.context";
 import { BrowserRouter } from "react-router-dom";
 
-
 ReactDOM.render(
-   <React.StrictMode>
-      <BrowserRouter>
-         <ContextProvider>
-            <App />
-         </ContextProvider>
-      </BrowserRouter>
-   </React.StrictMode>,
-   document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -3,55 +3,80 @@ import SliderRange from "../Components/Slider-range/index.js";
 import "../Style/formpage.css";
 
 function FormPage() {
-   return (
-      <div className="form-page-wrapper">
-         <div className="form-header">
-            <img
-               src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
-               alt="School of Code logo"
-               className="logo"
-               id="logo"
-            />
-            <div className="header-h1h2">
-               <h1>School of Code</h1>
-               <h2>Content Feedback</h2>
-            </div>
-         </div>
-         <div className="date-input">
-            <h6>Date</h6>
-            <input placeholder="dd/mm/yyyy" />
-         </div>
-         <h6>Rate your experience</h6>
-         <div className="rate-experience">
-            <p className="range-text">Inadequate</p>
-            <SliderRange max="5" min="0" />
-            <p className="range-text">Excellent</p>
-         </div>
-         <h6>
-            If there was a guest speaker, rate the quality of the guest speaker:
-         </h6>
-         <div className="guest-speaker-experience">
-            <p className="range-text">Inadequate</p>
-            <SliderRange max="5" min="0" />
-            <p className="range-text">Excellent</p>
-         </div>
-         <h6>How are you feeling emotionally?</h6>
-         <div className="rate-emotionally-feeling">
-            <p className="emoji">😢</p>
-            <div className="slider-wrapper">
-               <SliderRange max="5" min="0" />
-            </div>
-            <p className="emoji">😁</p>
-         </div>
-         <div className="review-section">
-            <h6>
-               Why did you choose the rating(s) above? What did we do well, and
-               what can we improve on?{" "}
-            </h6>
-            <textarea placeholder="Your answer" />
-         </div>
+  const formURL =
+    "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfr3RK6pqOgeAUSFt_ZisY2dSQA9QT4FBSjFglij476t1nypQ/formResponse";
+  const formResponse = {
+    "entry.241441914_sentinel": "am/pm answer here",
+    "entry.779781745_sentinel": "rate your experience answer here",
+    "entry.1719766692_sentinel": "guest speaker answer here",
+    "entry.2105669134_sentinel": "emotion answer here",
+    "couldnt find yet": "anything else answer here",
+  }; //construct object to send from values entered into our form
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log("submit form");
+    //construct form response and send to google forms here
+  }
+
+  return (
+    <div className="form-page-wrapper">
+      <div className="form-header">
+        <img
+          src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
+          alt="School of Code logo"
+          className="logo"
+          id="logo"
+        />
+        <div className="header-h1h2">
+          <h1>School of Code</h1>
+          <h2>Content Feedback</h2>
+        </div>
       </div>
-   );
+      <div className="date-input">
+        <h6>Date</h6>
+        <input placeholder="dd/mm/yyyy" />
+      </div>
+      <div className="ampm-session">
+        <h6>What session was this - morning or afternoon?</h6>
+        <p className="range-session">Morning</p>
+        <SliderRange max="1" min="2" />
+        <p className="range-session">Afternoon</p>
+      </div>
+      <h6>Rate your experience</h6>
+      <div className="rate-experience">
+        <p className="range-text">Inadequate</p>
+        <SliderRange max="5" min="0" />
+        <p className="range-text">Excellent</p>
+      </div>
+      <h6>
+        If there was a guest speaker, rate the quality of the guest speaker:
+      </h6>
+      <div className="guest-speaker-experience">
+        <p className="range-text">Inadequate</p>
+        <SliderRange max="5" min="0" />
+        <p className="range-text">Excellent</p>
+      </div>
+      <h6>How are you feeling emotionally?</h6>
+      <div className="rate-emotionally-feeling">
+        <p className="emoji">😢</p>
+        <div className="slider-wrapper">
+          <SliderRange max="5" min="0" />
+        </div>
+        <p className="emoji">😁</p>
+      </div>
+      <div className="review-section">
+        <h6>
+          Why did you choose the rating(s) above? What did we do well, and what
+          can we improve on?{" "}
+        </h6>
+        <textarea placeholder="Your answer" />
+      </div>
+      <input type="submit" onClick={handleSubmitForm}>
+        Submit
+      </input>
+    </div>
+  );
 }
 
 export default FormPage;
