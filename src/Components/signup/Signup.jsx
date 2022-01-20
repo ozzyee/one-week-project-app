9128ab4f381d5d6b6b6c5ebced6ef2a5f64585a7
Luna Yu <yuanluna.yu@hotmail.it>
2022-01-20 11:49:02 +0000

    css almost done for form page

--- src/Components/signup/Signup.jsx
+++ src/Components/signup/Signup.jsx
@@ -19,7 +19,7 @@ function SignUp() {
       <div id="fixed-screen">
          {/* Background */}
          <div className="bg-img-wrapper">
-            <img src="/space-4984262.jpg" className="bg-img" alt="bg"/>
+            <img src="/space-4984262.jpg" className="bg-img" alt="bg" />
             <div className="overlay" />
          </div>
          <section id="login">
--- src/pages/formpage.js
+++ src/pages/formpage.js
@@ -1,27 +1,54 @@
 import React from "react";
 import SliderRange from "../Components/Slider-range/index.js";
-import '../Style/formpage.css';
+import "../Style/formpage.css";
 
-function FormPage(){
-    return (
-        <div className="form-page-wrapper">
-            <div className="rate-experience">
-                <p className="range-text">Inadequate</p>
-                <SliderRange max='5' min='0' />
-                <p className="range-text">Excellent</p>
+function FormPage() {
+   return (
+      <div className="form-page-wrapper">
+         <div className="form-header">
+            <img
+               src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
+               alt="School of Code logo"
+               className="logo"
+               id="logo"
+            />
+            <h1>School of Code Content Feedback</h1>
+         </div>
+         <div className="date-input">
+            <h6>Date</h6>
+            <input placeholder="dd/mm/yyyy" />
+         </div>
+         <h6>Rate your experience</h6>
+         <div className="rate-experience">
+            <p className="range-text">Inadequate</p>
+            <SliderRange max="5" min="0" />
+            <p className="range-text">Excellent</p>
+         </div>
+         <h6>
+            If there was a guest speaker, rate the quality of the guest speaker:
+         </h6>
+         <div className="guest-speaker-experience">
+            <p className="range-text">Inadequate</p>
+            <SliderRange max="5" min="0" />
+            <p className="range-text">Excellent</p>
+         </div>
+         <h6>How are you feeling emotionally?</h6>
+         <div className="rate-emotionally-feeling">
+            <p className="emoji">😢</p>
+            <div className="slider-wrapper">
+               <SliderRange max="5" min="0" />
             </div>
-            <div className="guest-speaker-experience">
-                <p className="range-text">Inadequate</p>
-                <SliderRange max='5' min='0' />
-                <p className="range-text">Excellent</p>
-            </div>
-            <div className="rate-emotionally-feeling">
-                <p className="range-text">😢</p>
-                <SliderRange max='5' min='0' />
-                <p className="range-text">😁</p>
-            </div>
-        </div>
-    )
+            <p className="emoji">😁</p>
+         </div>
+         <div className="review-section">
+            <h6>
+               Why did you choose the rating(s) above? What did we do well, and
+               what can we improve on?{" "}
+            </h6>
+            <textarea placeholder="Your answer" />
+         </div>
+      </div>
+   );
 }
 
 export default FormPage;
